e961edb27c0f6ffde20fb2ca016fdac9cc6d9092
jml-student <student741@proton.me>
2024-09-13 12:40:26 +0200

    Set up html, events.js and dom.js

--- src/events.js
+++ src/events.js
@@ -1,1 +1,9 @@
 import { Player } from '../src/index'
+import { $, closeDialog } from '../src/dom'
+
+export function setUpEventListeners() {
+    const playerVsPlayer = $('.player-vs-player')
+    const playerVsComputer = $('.player-vs-computer')
+
+    playerVsPlayer.addEventListener('click', () => {})
+}
