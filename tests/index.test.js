b3d6053f09496e4df9b7583696faeb6708e57734
jml-student <160597100+jml-student@users.noreply.github.com>
2024-09-20 10:51:10 +0000

    Fix tests and add warning to run the tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,8 @@
+/*
+    COMMENT THE IMPORT STATEMENTS IN INDEX.JS TO RUN THESE TESTS.
+    OTHERWISE THERE WILL BE ENVIROMENT ISSUES.
+*/
+
 import { GameState, Gameboard, Player, Ship, getCells } from '../src/index.js'
 
 test('GameState creates and works correctly', () => {
@@ -25,86 +30,90 @@ test('Player creates correctly', () => {
 })
 
 test('Gameboard grid creates correctly', () => {
-    const gameboard = new Gameboard()
-    expect(gameboard.grid.length).toBe(100)
-    expect(gameboard.grid.every((cell) => cell === null)).toBe(true)
-    expect(gameboard.ships.length).toBe(0)
-    expect(gameboard.shot.length).toBe(0)
-    expect(gameboard.missed.length).toBe(0)
-    expect(gameboard.allSunk).toBe(false)
+    const player = new Player()
+    expect(player.gameboard.grid.length).toBe(100)
+    expect(player.gameboard.grid.every((cell) => cell === null)).toBe(true)
+    expect(player.gameboard.ships.length).toBe(0)
+    expect(player.gameboard.shot.length).toBe(0)
+    expect(player.gameboard.missed.length).toBe(0)
+    expect(player.gameboard.allSunk).toBe(false)
 })
 
 test('PlaceShip works correctly', () => {
-    const gameboard = new Gameboard()
-    gameboard.placeShip(55, 'vertical', 4)
-    expect(gameboard.ships.length).toBe(1)
+    const player = new Player()
+    player.gameboard.placeShip(55, 'vertical', 4)
+    expect(player.gameboard.ships.length).toBe(1)
 })
 
 test('Receive attack works correctly', () => {
-    const gameboard = new Gameboard()
-    gameboard.placeShip(0, 'horizontal', 3)
-    gameboard.receiveAttack(0)
-    gameboard.receiveAttack(1)
-    gameboard.receiveAttack(8)
-    expect(gameboard.ships.length).toBe(1)
-    expect(gameboard.grid[0].hits).toBe(2)
-    expect(gameboard.shot.includes(0)).toBe(true)
-    expect(gameboard.shot.includes(1)).toBe(true)
-    expect(gameboard.shot.includes(8)).toBe(true)
-    expect(gameboard.missed.includes(8)).toBe(true)
+    const player = new Player()
+    player.gameboard.placeShip(0, 'horizontal', 3)
+    player.gameboard.receiveAttack(0)
+    player.gameboard.receiveAttack(1)
+    player.gameboard.receiveAttack(8)
+    expect(player.gameboard.ships.length).toBe(1)
+    expect(player.gameboard.grid[0].hits).toBe(2)
+    expect(player.gameboard.shot.includes(0)).toBe(true)
+    expect(player.gameboard.shot.includes(1)).toBe(true)
+    expect(player.gameboard.shot.includes(8)).toBe(true)
+    expect(player.gameboard.missed.includes(8)).toBe(true)
 })
 
 test('Ship sunks and allSunk works', () => {
-    const gameboard = new Gameboard()
-    gameboard.placeShip(0, 'horizontal', 3)
-    gameboard.receiveAttack(0)
-    gameboard.receiveAttack(1)
-    gameboard.receiveAttack(2)
-    expect(gameboard.grid[0].sunk).toBe(true)
-    expect(gameboard.allSunk).toBe(true)
+    const player = new Player()
+    player.gameboard.placeShip(1, 'horizontal', 3)
+    player.gameboard.receiveAttack(0)
+    player.gameboard.receiveAttack(1)
+    player.gameboard.receiveAttack(2)
+    console.log(player)
+    expect(player.gameboard.ships[0].sunk).toBe(true)
+    expect(player.gameboard.allSunk).toBe(true)
 })
 
 test('Ship creates correctly', () => {
-    const ship = new Ship(3, 'horizontal')
-    expect(ship.length).toBe(3)
-    expect(ship.direction).toBe('horizontal')
-    expect(ship.hits).toBe(0)
-    expect(ship.sunk).toBe(false)
+    const player = new Player()
+    player.gameboard.placeShip(0, 'horizontal', 3)
+    expect(player.gameboard.ships[0].length).toBe(3)
+    expect(player.gameboard.ships[0].direction).toBe('horizontal')
+    expect(player.gameboard.ships[0].hits).toBe(0)
+    expect(player.gameboard.ships[0].sunk).toBe(false)
 })
 
 test('Ship receives hit', () => {
-    const ship = new Ship()
-    ship.hit()
-    expect(ship.hits).toBe(1)
+    const player = new Player()
+    player.gameboard.placeShip(0, 'horizontal', 3)
+    player.gameboard.ships[0].hit()
+    expect(player.gameboard.ships[0].hits).toBe(1)
 })
 
 test('CheckSunk works', () => {
-    const ship = new Ship(3, 'horizontal')
-    ship.hit()
-    ship.hit()
-    ship.hit()
-    ship.isSunk()
-    expect(ship.sunk).toBe(true)
+    const player = new Player()
+    player.gameboard.placeShip(0, 'horizontal', 3)
+    player.gameboard.ships[0].hit()
+    player.gameboard.ships[0].hit()
+    player.gameboard.ships[0].hit()
+    player.gameboard.ships[0].checkSunk()
+    expect(player.gameboard.ships[0].sunk).toBe(true)
 })
 
 test('GetCells works', () => {
     let index = 50
     let direction = 'vertical'
     let length = 6
-    expect(getCells(index, direction, length)).toBe([30, 40, 50, 60, 70, 80])
+    expect(getCells(index, direction, length)).toEqual([30, 40, 50, 60, 70, 80])
     length--
-    expect(expect(getCells(index, direction, length)).toBe([30, 40, 50, 60, 70]))
+    expect(expect(getCells(index, direction, length)).toEqual([30, 40, 50, 60, 70]))
     length--
-    expect(expect(getCells(index, direction, length)).toBe([40, 50, 60, 70]))
+    expect(expect(getCells(index, direction, length)).toEqual([40, 50, 60, 70]))
     length--
-    expect(expect(getCells(index, direction, length)).toBe([40, 50, 60]))
+    expect(expect(getCells(index, direction, length)).toEqual([40, 50, 60]))
     direction = 'horizontal'
     length = 6
-    expect(getCells(index, direction, length)).toBe([47, 48, 49, 50, 51, 52])
+    expect(getCells(index, direction, length)).toEqual([47, 48, 49, 50, 51, 52])
     length--
-    expect(getCells(index, direction, length)).toBe([48, 49, 50, 51, 52])
+    expect(getCells(index, direction, length)).toEqual([48, 49, 50, 51, 52])
     length--
-    expect(getCells(index, direction, length)).toBe([48, 49, 50, 51])
+    expect(getCells(index, direction, length)).toEqual([48, 49, 50, 51])
     length--
-    expect(getCells(index, direction, length)).toBe([49, 50, 51])
+    expect(getCells(index, direction, length)).toEqual([49, 50, 51])
 })
